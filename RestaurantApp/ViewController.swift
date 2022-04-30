e71aca271cac2f5ceaf8ec5fd756122734afcf2c
Carleton Snow <chip90@gmail.com>
2022-04-30 09:45:09 -0400

    Initial Commit

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by Carleton C Snow III on 4/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
